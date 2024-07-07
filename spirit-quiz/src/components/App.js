64ac92ab37f90713066b1f3c573a02eb0dcaec29
Rida Shiban <rsqwasert3@gmail.com>
2024-07-07 08:54:23 -0500

    first stable build with useState, mostly trash

--- spirit-quiz/src/components/App.js
+++ spirit-quiz/src/components/App.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import logo from '../images/logo.svg';
-import '../styles/App.css';
+import React, { useState } from "react";
+import logo from "../images/logo.svg";
+import "../styles/App.css";
 
 function App() {
+  const [selectedRadioButton, setSelectedRadioButton] =
+    useState("radioButton1");
+
+  const handleRadioButtonClick = (buttonNumber) => {
+    setSelectedRadioButton(buttonNumber);
+  };
+
   return (
     <div className="App">
+      <div>
+        <h1>With which spirit do you align?</h1>
+
+        <div>
+          <h2>
+            How much are you willing to scar the earth to say you saved it?
+          </h2>
+          <ul>
+            <li>
+              <input
+                type="radio"
+                id="radioButton1"
+                value="radioButton1"
+                checked={selectedRadioButton === "radioButton1"}
+                onChange={() => handleRadioButtonClick("radioButton1")}
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                id="radioButton2"
+                value="radioButton2"
+                checked={selectedRadioButton === "radioButton2"}
+                onChange={() => handleRadioButtonClick("radioButton2")}
+              />
+            </li>
+          </ul>
+          <p>A lot</p>
+          <p>The forest fire is a necessary evil</p>
+          <p>Sooner I would sacrifice myself</p>
+        </div>
+
+        <div>
+          <h2>
+            How much are you willing to scar the earth to say you saved it?
+          </h2>
+          <p>A lot</p>
+          <p>The forest fire is a necessary evil</p>
+          <p>Sooner I would sacrifice myself</p>
+          <p>The forest fire is a necessary evil</p>
+        </div>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
